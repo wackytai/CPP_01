c1fd80bfc603598d3a77f74d9fa76570b49e1c14
Tai Monteiro <48991630+wackytai@users.noreply.github.com>
2023-10-20 13:38:36 +0100

    corrected ex02

--- ex02/main.cpp
+++ ex02/main.cpp
@@ -7,7 +7,7 @@ int main(void)
     std::string& stringREF = str;
 
     std::cout << "- str memory address: " << &str << std::endl;
-    std::cout << "- stringPTR memory address: " << &stringPTR << std::endl;
+    std::cout << "- stringPTR memory address: " << stringPTR << std::endl;
     std::cout << "- stringREF memory address: " << &stringREF << std::endl;
     std::cout << "- str value: " << str << std::endl;
     std::cout << "- stringPTR value: " << *stringPTR << std::endl;

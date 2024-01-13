e84c04fee56ca62e53bb7c0d0e3396984b636d51
wackytai <tainahmonteiro.info@gmail.com>
2024-01-13 17:15:24 +0000

    necessary flags

--- ex06/main.cpp
+++ ex06/main.cpp
@@ -1,10 +1,27 @@
 #include "Harl.hpp"
 
-int	main(int argc, char **argv)
+int	main(int ac, char **av)
 {
-	Harl	myHarl;
-	if (argc != 2)
-		return 1;
-	myHarl.complain(argv[1]);
-	return 0;
+	Harl	Karen;
+	
+	if (ac != 2)
+		return (1);
+	Karen.complain(av[1]);
+	return (0);
 }
+
+/* int	main(void)
+{
+	Harl	Karen;
+	Karen.complain("DEBUG");
+	Karen.complain("DEBUG1");
+	Karen.complain("INFO");
+	Karen.complain("1INFO");
+	Karen.complain("WARNING");
+	Karen.complain("warning");
+	Karen.complain("ERROR");
+	Karen.complain("Error");
+	Karen.complain("123");
+	Karen.complain("");
+	Karen.complain(std::string());
+} */
